db1e392f1de8282d3c25d5b6f66dd422eac2e2e2
Liusj <1690150504@qq.com>
2023-02-02 13:12:27 +0800

    feat:图片流

--- socket/index.js
+++ socket/index.js
@@ -6,25 +6,30 @@ const clients = [] // 连接池
 wss.on('connection', function connection(ws) {
   clients.push(ws)
   ws.on('message', function message(data) {
-    const { type, ...user } = JSON.parse(data)
-    if (type === 0) { // 退出连接
-      clients.forEach(w => {
-        w.send(JSON.stringify({ payload: user.nickname, type }))
-      })
-    }
-    if (type === 1) { // 初次进入
-      clients.forEach(w => {
-        w.send(JSON.stringify({ payload: user.nickname, type }))
-      })
-    }
-    if (type === 2) { // 发送消息
-      const date = new Date()
-      const hours = date.getHours()
-      const minutes = date.getMinutes()
-      clients.forEach(w => {
-        w.send(JSON.stringify({ payload: { ...user, time: `${hours}:${minutes}` }, type }))
-      })
-    }
+      const { type, ...user } = JSON.parse(data)
+      if (type === 0) { // 退出连接
+        clients.forEach(w => {
+          w.send(JSON.stringify({ payload: user.nickname, type }))
+        })
+      }
+      if (type === 1) { // 初次进入
+        clients.forEach(w => {
+          w.send(JSON.stringify({ payload: user.nickname, type }))
+        })
+      }
+      if (type === 2) { // 发送消息
+        const date = new Date()
+        const hours = date.getHours()
+        const minutes = date.getMinutes()
+        clients.forEach(w => {
+          w.send(JSON.stringify({ payload: { ...user, time: `${hours}:${minutes}` }, type }))
+        })
+      }
+      if (type === 3) { // 图片传输
+        clients.forEach(w => {
+          w.send(JSON.stringify({ payload: { ...user }, type }))
+        })
+      }
   })
 })
 
