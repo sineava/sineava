fabb23cc39048d1ef6275021433384360c7eaf72
Liusj <1690150504@qq.com>
2023-02-02 16:19:30 +0800

    feat:调整连接池数量

--- socket/index.js
+++ socket/index.js
@@ -1,38 +1,45 @@
 import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 })
-const clients = [] // 连接池
+let clients = [] // 连接池
 
 wss.on('connection', function connection(ws, req) {
-  clients.push(ws)
   ws.on('message', function message(data) {
-      const { type, ...user } = JSON.parse(data)
-      if (type === 0) { // 退出连接
-        clients.forEach(w => {
-          w.send(JSON.stringify({ payload: user.nickname, type }))
-        })
+    const { type, ...user } = JSON.parse(data)
+    if (type === 0) { // 退出连接
+      clients = clients.filter(client => client.username !== user.username)
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: user.nickname, type }))
+      })
+    }
+    if (type === 1) { // 初次进入
+      const index = clients.findIndex(client => client.username === user.username)
+      ws.username = user.username
+      if (index === -1) {
+        clients.push(ws)
+      } else {
+        clients[index] = ws
       }
-      if (type === 1) { // 初次进入
-        ws.username = user.username
-        clients.forEach(w => {
-          w.send(JSON.stringify({ payload: user.nickname, type }))
-        })
-      }
-      if (type === 2) { // 发送消息
-        const date = new Date()
-        const hours = date.getHours()
-        const minutes = date.getMinutes()
-        clients.forEach(w => {
-          w.send(JSON.stringify({ payload: { ...user, time: `${hours}:${minutes}` }, type }))
-        })
-      }
-      if (type === 3) { // 图片传输
-        clients.forEach(w => {
-          if (w.username !== user.username) {
-            w.send(JSON.stringify({ payload: { ...user }, type }))
-          }
-        })
-      }
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: user.nickname, type }))
+      })
+    }
+    if (type === 2) { // 发送消息
+      const date = new Date()
+      const hours = date.getHours()
+      const minutes = date.getMinutes()
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: { ...user, time: `${hours}:${minutes}` }, type }))
+      })
+    }
+    if (type === 3) { // 图片传输
+      clients.forEach(w => {
+        if (w.username !== user.username) {
+          w.send(JSON.stringify({ payload: { ...user }, type }))
+        }
+      })
+    }
+    console.log(clients.length)
   })
 })
 
