8719fdfcc16b40d789393e892cbba34d473f56dd
Liusj <1690150504@qq.com>
2023-02-02 11:36:23 +0800

    feat:聊天室

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,33 @@
 import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 })
+const clients = [] // 连接池
 
 wss.on('connection', function connection(ws) {
+  clients.push(ws)
   ws.on('message', function message(data) {
-    console.log('received: %s', data)
+    const { type, ...user } = JSON.parse(data)
+    if (type === 0) { // 退出连接
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: user.nickname, type }))
+      })
+    }
+    if (type === 1) { // 初次进入
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: user.nickname, type }))
+      })
+    }
+    if (type === 2) { // 发送消息
+      const date = new Date()
+      const hours = date.getHours()
+      const minutes = date.getMinutes()
+      clients.forEach(w => {
+        w.send(JSON.stringify({ payload: { ...user, time: `${hours}:${minutes}` }, type }))
+      })
+    }
   })
+})
 
-  ws.send('用户已连接')
+wss.on('close', function close(ws) {
+  console.log(ws)
 })
